6a963f5b029523d9c0a7e894807e44ff7d9edd37
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-03-27 15:12:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import dice from "./Assets/images/icon-dice.svg";
+import dividerDesk from "./Assets/images/pattern-divider-desktop.svg";
+import dividerMobile from "./Assets/images/pattern-divider-mobile.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="box">
+        <h2 className="head">Quote #1</h2>
+        <p className="advice">
+          &#8220; shq-e-mehboob ma chahiye mehfil eik hi hasti k sath // saans
+          nahi aata aur badal jaata ha khoon ka rang, jab aye masti ki raat
+          &#8221;
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={dividerDesk} alt="" className="divider desk" />
+        <img src={dividerMobile} alt="" className="divider mob" />
+        <div className="dice-div">
+          <img src={dice} alt="" className="dice" />
+        </div>
+      </div>
     </div>
   );
 }
